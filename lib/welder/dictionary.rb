618ac09851afc8de6bea72d6c5fec65d503771c4
Ryan Greenberg <ryangreenberg@gmail.com>
2012-02-10 18:37:47 -0800

    Excludes long words from dictionary

--- lib/welder/dictionary.rb
+++ lib/welder/dictionary.rb
@@ -1,7 +1,8 @@
 require 'set'
 
 class Welder::Dictionary
-  def initialize(words)
+  def initialize(words, max_length = nil)
+    words.reject! {|w| w.size > max_length } if max_length
     @words = Set.new(words)
   end
 
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -4,13 +4,24 @@ describe Welder::Dictionary do
   before do
     @words = ['apple', 'banana', 'cumquat']
   end
-  
+
+  describe "#initialize" do
+    it "excludes words longer than the max_length" do
+      short_word = 'kiwi'
+      long_word = 'apple'
+      words = [ short_word, long_word ]
+      dictionary = Welder::Dictionary.new(words, 4)
+      dictionary.should include(short_word)
+      dictionary.should_not include(long_word)
+    end
+  end
+
   describe "#include?" do
     it "returns true if the dictionary was initialized with the given word" do
       dictionary = Welder::Dictionary.new(@words)
       dictionary.include?(@words.first).should be_true
     end
-    
+
     it "returns false if the dictionary was not initialized with the given word" do
       dictionary = Welder::Dictionary.new(@words)
       dictionary.include?('durian').should be_false
