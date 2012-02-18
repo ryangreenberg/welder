d80cd6b97778a7edf17821c5bdbf73bd21324190
Ryan Greenberg <ryangreenberg@gmail.com>
2012-02-18 10:31:55 -0800

    Adds scoring to Game#swap logic

--- lib/welder/game.rb
+++ lib/welder/game.rb
@@ -14,28 +14,51 @@ class Welder::Game
 
   def populate
     @board.populate do |x, y|
-      Welder::Tile.new(@tile_generator.get_tile)
+      @tile_generator.get_tile
+    end
+  end
+
+  def populate_empty
+    @board.populate do |x, y|
+      if @board.get_tile(x, y).empty?
+        @tile_generator.get_tile
+      else
+        false
+      end
     end
   end
 
   def valid_words_on_board
-    possible_words = @board.possible_words
+    possible_words = @board.possible_words(@rules.min_word_length)
     possible_words.select do |word|
       @dictionary.include?(word.to_s)
     end
   end
 
   def remove_valid_words
-    words_to_remove = valid_words_on_board
-    words_to_remove.each do |word|
+    # TODO Detect words entirely contained by other words
+    valid_words_on_board.each do |word|
       @board.remove_word(word)
     end
-    words_to_remove
+  end
+
+  def ensure_no_valid_words
+    while valid_words_on_board.size > 0
+      remove_valid_words
+      populate_empty
+    end
   end
 
   def swap(x, y, direction)
-    @board.swap(x, y, directory)
-    remove_valid_words
-    @board.drop_tiles
+    if @board.swap(x, y, directory)
+      @swaps -= 1
+      add_to_score(remove_valid_words)
+      @board.drop_tiles
+    end
+  end
+
+  def add_to_score(words)
+    @score += words.inject(0) {|sum, word| sum += word.score}
+    @score += (words.size - 1) * @rules.multiple_word_bonus
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,14 +5,15 @@ describe Welder::Game do
     @board_size = 4
     words = ['abc', 'def', 'ghi']
     @dictionary = Welder::Dictionary.new(words)
+    @rules = Welder::Rules.new(:min_word_length => 3)
     @tile_generator = Welder::TileGenerator.new([['a', 1]])
   end
 
   describe "#populate" do
     it "fills the board with tiles" do
-      game = Welder::Game.new(@board_size, @dictionary, @tile_generator)
+      game = Welder::Game.new(@board_size, @dictionary, @rules, @tile_generator)
       game.board.each_row do |row|
-        row.compact.should be_empty
+        row.reject {|tile| tile.empty? }.should be_empty
       end
       game.populate
       game.board.each_row do |row|
@@ -20,6 +21,71 @@ describe Welder::Game do
           cell.should be_a(Welder::Tile)
         end
       end
+    end
+  end
+
+  describe "#populate_empty" do
+    it "fills only empty tiles on the board" do
+      game = Welder::Game.new(@board_size, @dictionary, @rules, @tile_generator)
+      existing_tile = Welder::Tile.new('a')
+      game.board.set_tile(0, 0, existing_tile)
+      game.populate_empty
+
+      game.board.each_row do |row|
+        row.each do |cell|
+          cell.should be_a(Welder::Tile)
+          cell.should_not be_empty
+        end
+      end
+      game.board.get_tile(0, 0).should == existing_tile
+    end
+  end
+
+  describe "#ensure_no_valid_words" do
+    it "changes the board so that valid_words_on_board is an empty array" do
+      game = Welder::Game.new(@board_size, @dictionary, @rules, @tile_generator)
+
+      # Place valid words on the board
+      game.board.set_tile(0, 0, 'a')
+      game.board.set_tile(0, 1, 'b')
+      game.board.set_tile(0, 2, 'c')
+
+      game.board.set_tile(1, 0, 'd')
+      game.board.set_tile(2, 0, 'e')
+      game.board.set_tile(3, 0, 'f')
+
+      game.valid_words_on_board.size.should == 2
+
+      game.ensure_no_valid_words
+
+      game.valid_words_on_board.size.should == 0
+    end
+  end
+
+  describe "#add_to_score" do
+    it "adds the score of the word to the game score" do
+      game = Welder::Game.new(@board_size, @dictionary, @rules, @tile_generator)
+      game.populate
+      game.score.should == 0
+      word = game.board.get_word(0, 0, :horizontal, 4)
+      game.add_to_score([ word ])
+      game.score.should == word.score
+    end
+
+    it "adds the multiple_word_bonus for each word in excess of one" do
+      game = Welder::Game.new(@board_size, @dictionary, @rules, @tile_generator)
+      game.populate
+      game.score.should == 0
+      words = [
+        game.board.get_word(0, 0, :horizontal, 4),
+        game.board.get_word(0, 0, :vertical, 4),
+        game.board.get_word(0, 1, :horizontal, 4)
+      ]
+      words_score = words.inject(0){ |sum, word| sum += word.score }
+      multiple_word_bonus = @rules.multiple_word_bonus * 2 # 2 extra words
+
+      game.add_to_score(words)
+      game.score.should == words_score + multiple_word_bonus
     end
   end
 end
